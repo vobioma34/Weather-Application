43952404d9df87951fa01bfc33b9e1ee2ae725bd
Victor Obioma <vobioma34@icloud.com>
2023-07-28 15:55:42 -0500

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Victor Obioma on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
